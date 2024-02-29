6c7c4282ab1c2a4c3135151ebb8ca747d750a00f
Chunsen Wang <chunsenwang@tubi.tv>
2024-02-29 15:04:10 +0800

    add pre and after customer code config

--- src/main/scala/com/tubitv/CustomizeSourceCodeGenerator.scala
+++ src/main/scala/com/tubitv/CustomizeSourceCodeGenerator.scala
@@ -5,6 +5,8 @@ import slick.codegen.SourceCodeGenerator
 
 class CustomizeSourceCodeGenerator(model: m.Model, config: CodeGenConfig) extends SourceCodeGenerator(model) {
   override def packageCode(profile: String, pkg: String, container: String, parentType: Option[String]): String = {
+    def customizeHeadCode = config.extraHeadCode.map(code => s"\n$code\n").getOrElse("")
+    def customizeTailCode = config.extraTailCode.map(code => s"\n$code\n").getOrElse("")
     s"""
        |package $pkg
        |
@@ -16,12 +18,12 @@ class CustomizeSourceCodeGenerator(model: m.Model, config: CodeGenConfig) extend
        | * Please do not touch this file manually, use slick-codegen
        | */
        |object $container extends {
-       |  ${indent(config.extraImports.map(i => s"import ${i}").mkString("\n"))}
+       |  ${indent(config.extraImports.map(i => s"import ${i}").mkString("\n") + customizeHeadCode)}
        |
        |  val profile = ${config.profile}
        |  import profile.api._
        |
-       |  ${indent(code)}
+       |  ${indent(code + customizeTailCode)}
        |}""".stripMargin.trim()
 
   }
